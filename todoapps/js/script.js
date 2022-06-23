4211b9f0a5b6cfd0ea36a84350fb549b8d25b6d3
Jaykhalid <jaydanekhalid@gmail.com>
2022-06-23 18:29:46 +0700

    todoApps tested

---
+++ todoapps/js/script.js
@@ -0,0 +1,40 @@
+const todos        = [];
+const RENDER_EVENT = 'render-todo';
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const submitForm   = document.getElementById('form');
+    submitForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        addTodo();
+    });
+
+    function addTodo() {
+        const textTodo    = document.getElementById('title').value;
+        const timestamp   = document.getElementById('date').value;
+
+        const generatedID = generateId();
+        const todoObject  = generateTodoObject(generatedID, textTodo, timestamp, false);
+        todos.push(todoObject);
+
+        document.dispatchEvent(new Event(RENDER_EVENT));
+    }
+
+    function generateId() {
+        return Math.floor(Math.random() * 1000000);
+
+    }
+
+    function generateTodoObject(id, task, timestamp, isCompleted) {
+        return {
+            id, 
+            task,
+            timestamp, 
+            isCompleted
+        }
+    }
+});
+
+document.addEventListener(RENDER_EVENT, function() {
+    console.log(todos);
+})
